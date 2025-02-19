518fcb8ca28e3ed46f6d37c09797ca2c9e016af4
Nilav <nilavtalukdar06@gmail.com>
2025-02-19 19:17:34 +0530

    Added root layout

---
+++ app/metadata.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "EcoSwachh",
+  description:
+    "Empowering waste management with smart solutions for a cleaner, digital India, supporting Swachh Bharat Abhiyan's vision.",
+};
