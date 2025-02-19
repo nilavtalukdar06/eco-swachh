0920e09f2316ec9a00d5f01e47c3c4e505785feb
Nilav <nilavtalukdar06@gmail.com>
2025-02-20 02:11:28 +0530

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Toaster } from "react-hot-toast";
 import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "EcoSwachh",
-  description:
-    "Empowering waste management with smart solutions for a cleaner, digital India, supporting Swachh Bharat Abhiyan's vision.",
-};
 
 export default function RootLayout({
   children,
