ab58cff2553178b409a52c9a6c2978f641754f98
Nilav <nilavtalukdar06@gmail.com>
2025-03-06 08:57:34 +0530

    Updated the ui of the donate us form

--- app/donateus/page.tsx
+++ app/donateus/page.tsx
@@ -2,7 +2,7 @@ import PaymentForm from "@/components/Form";
 
 export default function DonateUs() {
   return (
-    <div className="bg-white">
+    <div className="bg-transparent">
       <PaymentForm />
     </div>
   );
