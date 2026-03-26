1c2bcace03d69f2758ec00f8184549f2bbd1b5e6
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-03-26 22:21:06 +0530

    added procedures to ban and unban user

--- apps/admin/features/users/server/user-procedures.ts
+++ apps/admin/features/users/server/user-procedures.ts
@@ -1,6 +1,9 @@
 import { protectedProcedure, createTRPCRouter } from "@/dal/init";
+import { auth } from "@/lib/auth";
 import { TRPCError } from "@trpc/server";
 import { prisma } from "@workspace/db";
+import { headers } from "next/headers";
+import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async (opts) => {
@@ -21,4 +24,49 @@ export const userRouter = createTRPCRouter({
     });
     return result;
   }),
+  banUser: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string().uuid({ message: "the user id is not valid" }),
+        banReason: z
+          .string()
+          .min(5, { message: "ban reason is too short" })
+          .max(200, { message: "ban reason is too long" }),
+      }),
+    )
+    .mutation(async (opts) => {
+      const role = opts.ctx.user.role;
+      if (role !== "admin") {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "you are not authorized to perform this action",
+        });
+      }
+      await auth.api.banUser({
+        body: {
+          userId: opts.input.userId,
+          banReason: opts.input.banReason,
+        },
+        headers: await headers(),
+      });
+    }),
+  unbanUser: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string().uuid({ message: "the user id is not valid" }),
+      }),
+    )
+    .mutation(async (opts) => {
+      const role = opts.ctx.user.role;
+      if (role !== "admin") {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "you are not authorized to perform this action",
+        });
+      }
+      await auth.api.unbanUser({
+        body: { userId: opts.input.userId },
+        headers: await headers(),
+      });
+    }),
 });
